e87d32d6b4a77bdedf3d4a203c6a5a3abe88f8f1
aldoram5 <pedro.rangel@gbmobile.com>
2014-12-29 13:07:18 -0600

    Added OpenCV to Project

--- app/src/main/java/com/duckmedia/ndksample/MyActivity.java
+++ app/src/main/java/com/duckmedia/ndksample/MyActivity.java
@@ -1,21 +1,31 @@
 package com.duckmedia.ndksample;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
+import android.view.SurfaceView;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Mat;
 
 
-public class MyActivity extends ActionBarActivity {
+public class MyActivity extends Activity implements CameraBridgeViewBase.CvCameraViewListener2{
+    private CameraBridgeViewBase mOpenCvCameraView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i("ASDA", "called onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
-
-        TextView textView = (TextView) findViewById(R.id.textview);
-        textView.setText(hello());
+        mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.HelloOpenCvView);
+        mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
+        mOpenCvCameraView.setMaxFrameSize(2048,2048);
+        mOpenCvCameraView.setCvCameraViewListener(this);
     }
 
     @Override
@@ -40,6 +50,56 @@ public class MyActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
+
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS:
+                {
+                    Log.i(TAG, "OpenCV loaded successfully");
+                    mOpenCvCameraView.enableView();
+                } break;
+                default:
+                {
+                    super.onManagerConnected(status);
+                } break;
+            }
+        }
+    };
+
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
+    }
+
+    public void onDestroy() {
+        super.onDestroy();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
+    }
+
+    public void onCameraViewStarted(int width, int height) {
+    }
+
+    public void onCameraViewStopped() {
+    }
+
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        return inputFrame.rgba();
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        if(OpenCVLoader.initDebug())
+            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        //OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_6, this, mLoaderCallback);
+    }
 
     static {
         System.loadLibrary("hello");
